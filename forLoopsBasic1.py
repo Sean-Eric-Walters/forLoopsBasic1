528749c5887f7de45c0944a4150e2b45c490629c
Sean-Eric-Walters <seanericwalters@gmail.com>
2021-06-07 17:58:04 -0700

    ch1

--- forLoopsBasic1.py
+++ forLoopsBasic1.py
@@ -16,7 +16,7 @@ for i in range(1, 101, 1):
 
 #Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 sum = 0
-for i in range (1, 500001, 3):
+for i in range (0, 500001, 2):
     sum = sum + i
 print(sum)
 
